9f60f4144ba09107a26fa79e2d0d30e1dfa8d940
ameliendrez <ameliendrez@gmail.com>
2018-10-24 19:26:33 -0300

    Iniciando tests

---
+++ TrabajoEspecial/src/testJUnit/TestsRunner.java
@@ -0,0 +1,11 @@
+package testJUnit;
+import org.junit.runner.JUnitCore;
+
+
+public class TestsRunner {
+
+	 public static void main(String args[]) {
+	        JUnitCore.runClasses(TestSuite.class);
+	    }
+	
+}
--- TrabajoEspecial/src/testJUnit/UsuarioTest.java
+++ TrabajoEspecial/src/testJUnit/UsuarioTest.java
@@ -1,7 +1,10 @@
 package testJUnit;
 
+import entidades.Usuario;
 import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -11,27 +14,78 @@ import entidades.Usuario;
 
 public class UsuarioTest {
 	
-	Usuario usuario;
-	Lugar uncpba;
-
+	private Usuario usuario;
+	private Lugar uncpba;
+	
+	
 	@BeforeClass
-	public void createUsuario(){
+	public static void setUpBeforeClass() throws Exception {
+		System.out.println("Usuario -> setupBeforeClass");
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	System.out.println("Usuario -> tearDownAfterClass");
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		System.out.println("Usuario -> Usuario");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	System.out.println("Usuario -> TearDownUsuario");
+	}
+	/**
+	 *  
+	 */
+	@Test
+	public void testUsuario() {
+	System.out.println("Usuario -> TestUsuario");
+	}
+
+	@Before
+	public void testCreateUsuario() {
 		this.uncpba = new Lugar(1, "uncpba", "Tandil");
 		this.usuario = new Usuario(36626800, "Agustin", "Meliendrez", uncpba);
+		System.out.println("Usuario -> testCreateUsuario");
 	}
-	
+
 	@Test
-	public void comprobarNombreCompleto(){
-		String nombre = this.usuario.getNombre();
-		assertEquals(nombre, "Agustin");
-		String apellido = this.usuario.getApellido();
-		assertEquals(apellido, "Meliendrez");
-	}
-	
-	@Test
-	public void comprobarDNI(){
+	public void testGetDni() {
 		int dni = this.usuario.getDni();
-		assertEquals(dni, 36626800);
+		System.out.println("Usuario -> testGetDni -> "+ dni);		
 	}
+	
+	@Test
+	public void testGetNombreCompleto() {
+		String apellido = this.usuario.getApellido();
+		String nombre = this.usuario.getNombre();
+		System.out.println("Usuario -> testGetNombreCompleto -> " + apellido + ", " + nombre);		
+	}
+	
+	@Test
+	public void testGetLugar() {
+		Lugar lugar = this.usuario.getLugar();
+		String nombreLugar = lugar.getNombre();
+		System.out.println("Usuario -> testGetLugar -> " + nombreLugar);		
+	}
+
+	@Test
+	public void testSetInicio() {
+		System.out.println("Usuario -> SetInicio");
+	}
+
+	@Test
+	public void testGetFin() {
+		//fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetFin() {
+		//fail("Not yet implemented");
+	}
+	
 	
 }
